b08c0303d970fa97669495f32909b41b796bdceb
horea.bucerzan <horea.bucerzan@gmail.com>
2019-08-23 00:23:29 +0300

    Initial commit

---
+++ src/main/java/startgame/StartGame.java
@@ -0,0 +1,38 @@
+package startgame;
+
+import libgdx.constants.GameIdEnum;
+import libgdx.implementations.iq.SkelGame;
+import libgdx.utils.startgame.test.DefaultAppInfoService;
+import libgdx.utils.startgame.test.DefaultBillingService;
+import libgdx.utils.startgame.test.DefaultFacebookService;
+
+public class StartGame {
+
+    public static void main(String[] args) {
+        SkelGame game = new SkelGame(
+                new DefaultFacebookService(),
+                new DefaultBillingService(),
+                new DefaultAppInfoService() {
+                    @Override
+                    public String getGameIdPrefix() {
+                        return GameIdEnum.skelgame.name();
+                    }
+
+                    @Override
+                    public float gameScreenTopMargin() {
+                        return super.gameScreenTopMargin();
+                    }
+
+                    @Override
+                    public String getAppName() {
+                        return "Crossword Garden";
+                    }
+
+                    @Override
+                    public String getLanguage() {
+                        return "ro";
+                    }
+                });
+        libgdx.utils.startgame.StartGame.main(game, args);
+    }
+}
