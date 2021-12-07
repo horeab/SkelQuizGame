9830b18721928414e2b1bd9ed498be73496b4cb4
horeab <horea.bucerzan@gmail.com>
2021-12-07 12:08:49 +0200

    moved package

---
+++ src/main/java/libgdx/LabelProcessor.java
@@ -0,0 +1,171 @@
+package libgdx;
+
+import com.google.gson.Gson;
+
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import libgdx.constants.Language;
+import libgdx.implementations.skelgame.GameIdEnum;
+
+class LabelProcessor {
+
+    public static void main(String[] args) {
+
+        List<GameIdEnum> gameIds = Arrays.asList(GameIdEnum.history);
+
+        Map<Pair<String, String>, String> defaultLabels = getLabelsForLanguage(gameIds, new HashMap<>(), Language.en);
+
+        Map<Pair<String, String>, String> labels = getLabelsForLanguage(gameIds, defaultLabels, Language.ro);
+
+        if (labels.size() != defaultLabels.size()) {
+            throw new RuntimeException("missing keys");
+        }
+
+        Map<String, String> labelsWithKeys = new TreeMap<>();
+        for (Map.Entry<Pair<String, String>, String> e : labels.entrySet()) {
+            labelsWithKeys.put(e.getKey().getRight(), e.getValue());
+        }
+        System.out.println(new Gson().toJson(labelsWithKeys));
+    }
+
+    private static Map<Pair<String, String>, String> getLabelsForLanguage(List<GameIdEnum> gameIds, Map<Pair<String, String>, String> defaultLabels, Language lang) {
+        String mainPath = getMainLabelsPath(lang);
+        String campPath = getMainGameLabelsPath(lang);
+
+        Map<Pair<String, String>, String> labels = new TreeMap<>();
+
+        addLabels(mainPath, null, labels);
+        addLabels(campPath, null, labels);
+
+        for (GameIdEnum gid : gameIds) {
+            String gamePath = getGameImplementationLabelsPath(gid);
+            addLabels(gamePath, lang, labels);
+        }
+
+        Map<Pair<String, String>, String> labelsWithOrigKeys = new TreeMap<>();
+        if (!defaultLabels.isEmpty()) {
+            for (Map.Entry<Pair<String, String>, String> e : labels.entrySet()) {
+                labelsWithOrigKeys.put(new MutablePair<>(e.getKey().getLeft(), getDefaultLanguageKey(defaultLabels, e.getKey().getLeft())), e.getValue());
+            }
+        }
+
+        return defaultLabels.isEmpty() ? labels : labelsWithOrigKeys;
+    }
+
+    private static String getDefaultLanguageKey(Map<Pair<String, String>, String> defaultLabels, String key) {
+
+        for (Map.Entry<Pair<String, String>, String> e : defaultLabels.entrySet()) {
+            if (e.getKey().getLeft().equals(key)) {
+                return e.getKey().getRight();
+            }
+        }
+
+        throw new RuntimeException("no key found " + key);
+    }
+
+    private static void addLabels(String path, Language lang, Map<Pair<String, String>, String> labels) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            String line = reader.readLine();
+
+
+            while (line != null) {
+
+                List<String> ignoreKeys = Arrays.asList("font_name", "privacy_policy", "facebook_share_btn");
+                List<String> onlyKeys = new ArrayList<>();
+
+                if (lang != null) {
+                    onlyKeys.add(lang + "_");
+                }
+
+                boolean ignoreBool = false;
+                for (String ignore : ignoreKeys) {
+                    if (line.startsWith(ignore)) {
+                        ignoreBool = true;
+                        break;
+                    }
+                }
+
+                for (String only : onlyKeys) {
+                    ignoreBool = true;
+                    if (line.startsWith(only)) {
+                        ignoreBool = false;
+                        break;
+                    }
+                }
+
+                if (ignoreBool) {
+                    line = reader.readLine();
+                    continue;
+                }
+
+                if (line.contains("=")) {
+                    String[] split = line.split("=");
+                    String value = split[1];
+
+                    value = value.replace("Highscore", "High Score");
+                    value = value.replace("\u0027", "'");
+                    value = value.replace("\\n", "\n");
+
+                    String key = value.toLowerCase();
+
+                    Map<String, String> replaceStrings = new HashMap<>();
+                    replaceStrings.put(" ", "_");
+                    replaceStrings.put("\n", "_");
+                    replaceStrings.put(":", "_");
+                    replaceStrings.put("-", "_");
+                    replaceStrings.put("'", "");
+                    replaceStrings.put("!", "");
+                    replaceStrings.put("+", "");
+                    replaceStrings.put("?", "");
+                    replaceStrings.put(".", "");
+                    replaceStrings.put("%", "");
+                    replaceStrings.put("{0}", "param0");
+                    replaceStrings.put("{1}", "param1");
+                    replaceStrings.put("{2}", "param2");
+
+                    for (Map.Entry<String, String> r : replaceStrings.entrySet()) {
+                        key = key.replace(r.getKey(), r.getValue());
+                    }
+                    key = "l_" + key;
+                    key = key.replace("__", "_");
+                    key = key.replace("__", "_");
+                    key = key.replace("__", "_");
+                    key = key.replace("__", "_");
+
+                    labels.put(new MutablePair<>(lang != null ? split[0].substring(split[0].indexOf("_") + 1).trim() : split[0].trim(), key.trim()), value);
+
+                }
+
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getGameImplementationLabelsPath(GameIdEnum gid) {
+        return "/Users/macbook/IdeaProjects/SkelQuizGame/src/main/resources/tournament_resources/implementations/" + gid.name() + "/labels/labels.properties";
+    }
+
+    private static String getMainGameLabelsPath(Language lang) {
+        return "/Users/macbook/IdeaProjects/SkelGame/src/main/resources/tournament_resources/main/labels/labels_" + lang + ".properties";
+    }
+
+    private static String getMainLabelsPath(Language lang) {
+        return "/Users/macbook/IdeaProjects/SkelQuizGame/src/main/resources/main_resources/main/labels/main_labels_" + lang + ".properties";
+    }
+}
---
+++ src/main/java/libgdx/QuestionProcessor.java
@@ -0,0 +1,79 @@
+package libgdx;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import libgdx.campaign.QuestionCategory;
+import libgdx.campaign.QuestionDifficulty;
+import libgdx.constants.Language;
+import libgdx.implementations.history.HistoryCategoryEnum;
+import libgdx.implementations.history.HistoryDifficultyLevel;
+
+class QuestionProcessor {
+
+    public static void main(String[] args) {
+
+        StringBuilder res = new StringBuilder();
+//        List<Language> languages = Arrays.asList(Language.en, Language.ro, Language.de);
+        List<Language> languages = Arrays.asList(Language.values());
+
+
+        for (Language language : languages) {
+            res.append("add" + language.name().toUpperCase() + "(result, questionConfig);\n");
+        }
+        res.append("    return result;\n" +
+                "  }\n");
+        for (Language language : languages) {
+
+            res.append(getQuestionsHeader(language));
+
+            for (QuestionDifficulty diff : HistoryDifficultyLevel.values()) {
+                for (QuestionCategory category : HistoryCategoryEnum.values()) {
+                    String qPath = "/Users/macbook/IdeaProjects/SkelQuizGame/src/main/resources/tournament_resources/implementations/history/questions/"
+                            + language + "/diff" + diff.getIndex() + "/questions_diff" + diff.getIndex() +
+                            "_cat" + category.getIndex() + ".txt";
+
+                    BufferedReader reader;
+                    List<String> questions = new ArrayList<>();
+                    try {
+                        reader = new BufferedReader(new FileReader(qPath));
+                        String line = reader.readLine();
+                        while (line != null) {
+                            questions.add("\"" + line + "\"");
+                            line = reader.readLine();
+                        }
+
+                        res.append(getQuestionsForCatAndDiff(diff, category, questions.toString()));
+
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            res.append("}");
+        }
+
+        System.out.println(res);
+    }
+
+    static String getQuestionsHeader(Language language) {
+        return "void add" + language.name().toUpperCase() + "(Map<Language, Map<CategoryDifficulty, List<Question>>> result,\n" +
+                "      HistoryGameQuestionConfig questionConfig) {\n" +
+                "    var language = Language." + language.name() + ";\n";
+    }
+
+    static String getQuestionsForCatAndDiff(QuestionDifficulty questionDifficulty, QuestionCategory questionCategory, String q) {
+
+        return "addQuestions(\n" +
+                "        result, //\n" +
+                "        language, //\n" +
+                "        questionConfig." + questionCategory.name() + ", //\n" +
+                "        questionConfig." + questionDifficulty.name().replace("_", "diff") + ", //\n" +
+                "        " + q + ");\n";
+    }
+}
